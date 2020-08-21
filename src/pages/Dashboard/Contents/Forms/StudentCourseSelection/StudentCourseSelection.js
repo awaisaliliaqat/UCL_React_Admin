57b57beb869e82e5de7f22765ba41e50d0c8f494
Farhan <farhantirmzi@gmail.com>
2020-08-21 16:08:02 +0500

    Farhan Chnages

--- src/pages/Dashboard/Contents/Forms/StudentCourseSelection/StudentCourseSelection.js
+++ src/pages/Dashboard/Contents/Forms/StudentCourseSelection/StudentCourseSelection.js
@@ -30,6 +30,7 @@ class StudentCourseSelection extends Component {
       regStatusId: 1,
       studentId: "",
       studentName: "",
+      isDownloadExcel:false
     };
   }
 
@@ -431,52 +432,51 @@ class StudentCourseSelection extends Component {
   };
 
   downloadExcelData = async () => {
-    alert("Coming Soon");
-  //     if (this.state.isDownloadExcel === false) {
-  //         this.setState({
-  //             isDownloadExcel: true
-  //         })
-  //         const type = this.state.applicationStatusId === 2 ? 'Submitted' : 'Pending';
-  //         const eventDataQuery = this.state.eventDate ? `&eventDate=${format(this.state.eventDate, "dd-MMM-yyyy")}` : '';
-  //         const url = `${process.env.REACT_APP_API_DOMAIN}/${process.env.REACT_APP_SUB_API_NAME}/academics/C02AdmissionsProspectApplication${type}ApplicationsExcelDownload?applicationId=${this.state.applicationId}&genderId=${this.state.genderId}&degreeId=${this.state.degreeId}&studentName=${this.state.studentName}${eventDataQuery}`;
-  //         await fetch(url, {
-  //             method: "GET",
-  //             headers: new Headers({
-  //                 Authorization: "Bearer " + localStorage.getItem("uclAdminToken")
-  //             })
-  //         })
-  //             .then(res => {
-  //                 if (res.status === 200) {
-  //                     return res.blob();
-  //                 }
-  //                 return false;
-  //             })
-  //             .then(
-  //                 json => {
-  //                     if (json) {
-  //                         var csvURL = window.URL.createObjectURL(json);
-  //                         var tempLink = document.createElement("a");
-  //                         tempLink.setAttribute("download", `Applications${type}.xlsx`);
-  //                         tempLink.href = csvURL;
-  //                         tempLink.click();
-  //                         console.log(json);
-  //                     }
-  //                 },
-  //                 error => {
-  //                     if (error.status === 401) {
-  //                         this.setState({
-  //                             isLoginMenu: true,
-  //                             isReload: false
-  //                         })
-  //                     } else {
-  //                         alert('Failed to fetch, Please try again later.');
-  //                         console.log(error);
-  //                     }
-  //                 });
-  //         this.setState({
-  //             isDownloadExcel: false
-  //         })
-  //     }
+   
+      if (this.state.isDownloadExcel === false) {
+          this.setState({
+              isDownloadExcel: true
+          })
+          
+          const url = `${process.env.REACT_APP_API_DOMAIN}/${process.env.REACT_APP_SUB_API_NAME}/common/C22CommonStudentsCourseSelectionBySessionIdExcelDownload?sessionId=${this.state.sessionId}`;
+          await fetch(url, {
+              method: "GET",
+              headers: new Headers({
+                  Authorization: "Bearer " + localStorage.getItem("uclAdminToken")
+              })
+          })
+              .then(res => {
+                  if (res.status === 200) {
+                      return res.blob();
+                  }
+                  return false;
+              })
+              .then(
+                  json => {
+                      if (json) {
+                          var csvURL = window.URL.createObjectURL(json);
+                          var tempLink = document.createElement("a");
+                          tempLink.setAttribute("download", `Applications.xlsx`);
+                          tempLink.href = csvURL;
+                          tempLink.click();
+                          console.log(json);
+                      }
+                  },
+                  error => {
+                      if (error.status === 401) {
+                          this.setState({
+                              isLoginMenu: true,
+                              isReload: false
+                          })
+                      } else {
+                          alert('Failed to fetch, Please try again later.');
+                          console.log(error);
+                      }
+                  });
+          this.setState({
+              isDownloadExcel: false
+          })
+      }
   }
 
   onSaveClick = () => {
