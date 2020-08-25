8075aa8e8511adec936aaba027000ce1f0da8dae
Muhammad Umar <umar.nadeem@hitechgroup.asia>
2020-08-25 20:19:16 +0500

    F41PDF icon and ajex call

--- src/pages/Dashboard/Contents/Forms/R41Reports.js
+++ src/pages/Dashboard/Contents/Forms/R41Reports.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import {Divider, IconButton, Tooltip, CircularProgress, Grid, Button} from "@material-ui/core";
 import {Typography, TextField, MenuItem} from "@material-ui/core";
 import ExcelIcon from "../../../../assets/Images/excel.png";
+import PDFIcon from "../../../../assets/Images/pdf_export_icon.png";
 import LoginMenu from "../../../../components/LoginMenu/LoginMenu";
 import { format } from "date-fns";
 import R41ReportsTableComponent from "./R41ReportsTableComponent";
@@ -238,6 +239,55 @@ class R41Reports extends Component {
     this.setState({isLoading: false});
   };
 
+  downloadPDFData = async () => {
+    alert("Coming Soon");
+  //     if (this.state.isDownloadExcel === false) {
+  //         this.setState({
+  //             isDownloadExcel: true
+  //         })
+  //         const type = this.state.applicationStatusId === 2 ? 'Submitted' : 'Pending';
+  //         const eventDataQuery = this.state.eventDate ? `&eventDate=${format(this.state.eventDate, "dd-MMM-yyyy")}` : '';
+  //         const url = `${process.env.REACT_APP_API_DOMAIN}/${process.env.REACT_APP_SUB_API_NAME}/academics/C02AdmissionsProspectApplication${type}ApplicationsExcelDownload?applicationId=${this.state.applicationId}&genderId=${this.state.genderId}&degreeId=${this.state.degreeId}&studentName=${this.state.studentName}${eventDataQuery}`;
+  //         await fetch(url, {
+  //             method: "GET",
+  //             headers: new Headers({
+  //                 Authorization: "Bearer " + localStorage.getItem("uclAdminToken")
+  //             })
+  //         })
+  //             .then(res => {
+  //                 if (res.status === 200) {
+  //                     return res.blob();
+  //                 }
+  //                 return false;
+  //             })
+  //             .then(
+  //                 json => {
+  //                     if (json) {
+  //                         var csvURL = window.URL.createObjectURL(json);
+  //                         var tempLink = document.createElement("a");
+  //                         tempLink.setAttribute("download", `Applications${type}.xlsx`);
+  //                         tempLink.href = csvURL;
+  //                         tempLink.click();
+  //                         console.log(json);
+  //                     }
+  //                 },
+  //                 error => {
+  //                     if (error.status === 401) {
+  //                         this.setState({
+  //                             isLoginMenu: true,
+  //                             isReload: false
+  //                         })
+  //                     } else {
+  //                         alert('Failed to fetch, Please try again later.');
+  //                         console.log(error);
+  //                     }
+  //                 });
+  //         this.setState({
+  //             isDownloadExcel: false
+  //         })
+  //     }
+  }
+
   getSectionIdFromCourseId = (courseId) => {
     let coursesMenuItems = this.state.coursesMenuItems;
     let res = coursesMenuItems.find((dt) => dt.id === courseId);
@@ -389,13 +439,6 @@ class R41Reports extends Component {
               */}
               Teacher Assigmnent Summary Report
             </Typography>
-            {/* 
-              <img alt="" src={ExcelIcon} onClick={() => this.downloadExcelData()} style={{
-                  height: 30, width: 32,
-                  cursor: `${this.state.isDownloadExcel ? 'wait' : 'pointer'}`,
-               }}
-              /> 
-            */}
             <div style={{ float: "right" }}>
               {/* 
               <Tooltip title="Search Bar">
@@ -414,6 +457,19 @@ class R41Reports extends Component {
                   <FilterIcon fontSize="default" color="primary" />
                 </IconButton>
               </Tooltip>
+              <Tooltip title="Export PDF">
+                <img 
+                  alt="" 
+                  src={PDFIcon} 
+                  onClick={() => this.downloadPDFData()} 
+                  style = {{
+                    height: 22, 
+                    width: 22,
+                    marginBottom: -7,
+                    cursor: `${this.state.isDownloadExcel ? 'wait' : 'pointer'}`,
+                  }}
+                />
+              </Tooltip> 
             </div>
           </div>
           <Divider
