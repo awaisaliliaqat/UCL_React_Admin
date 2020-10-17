509a5b49f536458e7686cc7bedc61c144236c484
Muhammad Umar <umar.nadeem@hitechgroup.asia>
2020-10-17 17:32:06 +0500

    R68ReportsAttendanceRecordSheet changed

--- src/pages/Dashboard/Contents/Forms/R68ReportsAttendanceRecordSheet.js
+++ src/pages/Dashboard/Contents/Forms/R68ReportsAttendanceRecordSheet.js
@@ -295,20 +295,18 @@ class DisplayAdmissionApplications extends Component {
               >
                 <TableHead>
                   <TableRow>
-                    <StyledTableCell
-                      style={{ borderLeft: "1px solid rgb(47, 87, 165)" }}
-                    >
-                      Course
-                    </StyledTableCell>
-                    <StyledTableCell align="center">Lectures</StyledTableCell>
-                    <StyledTableCell align="center">Tutorials</StyledTableCell>
-                    <StyledTableCell
-                      align="center"
-                      colSpan="1"
-                      style={{ borderRight: "1px solid rgb(47, 87, 165)" }}
-                    >
-                      Total
-                    </StyledTableCell>
+                    <StyledTableCell style={{ borderLeft: "1px solid rgb(47, 87, 165)" }}>Course</StyledTableCell>
+                    <StyledTableCell align="center" colSpan="2">Lectures</StyledTableCell>
+                    <StyledTableCell align="center" colSpan="2">Tutorials</StyledTableCell>
+                    <StyledTableCell align="center" colSpan="1" style={{ borderRight: "1px solid rgb(47, 87, 165)" }}>Total</StyledTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <StyledTableCell style={{ borderLeft: "1px solid rgb(47, 87, 165)" }}>&nbsp;</StyledTableCell>
+                    <StyledTableCell align="center">Schedule</StyledTableCell>
+                    <StyledTableCell align="center">Attended</StyledTableCell>
+                    <StyledTableCell align="center">Schedule</StyledTableCell>
+                    <StyledTableCell align="center">Attended</StyledTableCell>
+                    <StyledTableCell align="center" style={{ borderRight: "1px solid rgb(47, 87, 165)" }}>Total</StyledTableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -316,37 +314,16 @@ class DisplayAdmissionApplications extends Component {
                     this.state.tableData.map((row, index) => (
                       <Fragment key={"row" + row.teacherId + index}>
                         <TableRow>
-                          <StyledTableCell
-                            colSpan="4"
-                            style={{ backgroundColor: "#e1e3e8" }}
-                          >
-                            <b>{row.teacherLabel}</b>
-                          </StyledTableCell>
+                          <StyledTableCell colSpan="6" style={{ backgroundColor: "#e1e3e8" }}><b>{row.teacherLabel}</b></StyledTableCell>
                         </TableRow>
                         {row.teacherCourseData.map((row2, index2) => (
                           <TableRow key={"row" + row2.courseId + index2}>
-                            <StyledTableCell
-                              style={{
-                                borderLeft: "1px solid rgb(47, 87, 165)",
-                              }}
-                            >
-                              {row2.courseLabel}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {row2.attandanceCountLectures}
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                              {row2.attandanceCountTutorials}
-                            </StyledTableCell>
-                            <StyledTableCell
-                              align="center"
-                              style={{
-                                borderRight: "1px solid rgb(47, 87, 165)",
-                              }}
-                            >
-                              {row2.attandanceCountLectures +
-                                row2.attandanceCountTutorials}
-                            </StyledTableCell>
+                            <StyledTableCell style={{borderLeft: "1px solid rgb(47, 87, 165)"}}>{row2.courseLabel}</StyledTableCell>
+                            <StyledTableCell align="center">{row2.attandanceCountScheduledLectures}</StyledTableCell>
+                            <StyledTableCell align="center">{row2.attandanceCountAttendedLectures}</StyledTableCell>
+                            <StyledTableCell align="center">{row2.attandanceCountScheduledTutorials}</StyledTableCell>
+                            <StyledTableCell align="center">{row2.attandanceCountAttendedTutorials}</StyledTableCell>
+                            <StyledTableCell align="center" style={{borderRight: "1px solid rgb(47, 87, 165)"}}>{row2.attandanceCountLectures + row2.attandanceCountTutorials}</StyledTableCell>
                           </TableRow>
                         ))}
                       </Fragment>
