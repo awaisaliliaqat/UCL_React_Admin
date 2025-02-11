d2a92fbb92055dd50ae51ecdfb3f5fd0b001ab12
Muhammad Umar <umar.nadeem@hitechgroup.asia>
2025-02-11 17:31:23 +0500

    F337MonthlyEmployeeApproval change the defalt page 1st page insted of 2nd in view.

--- src/pages/Dashboard/Contents/Forms/MonthlyEmployeeApproval/F337MonthlyEmployeeApprovalReportTableComponent.js
+++ src/pages/Dashboard/Contents/Forms/MonthlyEmployeeApproval/F337MonthlyEmployeeApprovalReportTableComponent.js
@@ -129,7 +129,7 @@ class F337MonthlyEmployeeApprovalReportTableComponent extends Component {
           />
           {/* <SelectionState />  */}
           {/* <GroupingState defaultGrouping={[{ columnName: 'product' }]} defaultExpandedGroups={['EnviroCare Max']} /> */}
-          <PagingState defaultCurrentPage={1} defaultPageSize={10} />
+          <PagingState defaultCurrentPage={0} defaultPageSize={10} />
           <IntegratedFiltering />
           <IntegratedSorting />
           <IntegratedPaging />
