5166cf4c8852320964886a7575d4b708bd9102d5
Muhammad Umar <umar.nadeem@hitechgroup.asia>
2025-06-14 15:15:36 +0500

    refactor: Clean up formatting and improve readability in BottomBarWithViewColorBlue component

--- src/components/BottomBar/BottomBarWithViewColorBlue.js
+++ src/components/BottomBar/BottomBarWithViewColorBlue.js
@@ -7,91 +7,97 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  grow: {
-    flexGrow: 1,
-  },
+	grow: {
+		flexGrow: 1,
+	},
 }));
 
 function BottomBar(props) {
-  const classes = useStyles();
 
-  const bottomRightButtonAction = (props) => {
-    props.bottomRightButtonAction();
-  };
+	const classes = useStyles();
 
-  const bottomLeftButtonAction = (props) => {
-    props.bottomLeftButtonAction();
-  };
-  return (
-    <div>
-      <AppBar
-        position="fixed"
-        color="default"
-        style={{
-          top: "auto",
-          bottom: 0,
-          paddingLeft: props.isDrawerOpen ? 280 : 0,
-        }}
-      >
-        <Toolbar variant="dense">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => bottomLeftButtonAction(props)}
-            disabled={props.disableLeftButton}
-            style={{ display: props.leftButtonHide ? "none" : "block" }}
-          >
-            {props.leftButtonText}
-          </Button>
-          <div className={classes.grow} />
-          {props.otherActions}
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={props.disableRightButton || props.loading}
-            onClick={() => bottomRightButtonAction(props)}
-            style={{
-              display: props.hideRightButton ? "none" : "flex",
-            }}
-          >
-            {props.loading ? (
-              <CircularProgress size={24} style={{ color: "white" }} />
-            ) : (
-              props.right_button_text
-            )}
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	const bottomRightButtonAction = (props) => {
+		props.bottomRightButtonAction();
+	};
+
+	const bottomLeftButtonAction = (props) => {
+		props.bottomLeftButtonAction();
+	};
+	
+	return (
+		<div>
+			<AppBar
+				position="fixed"
+				color="default"
+				style={{
+					top: "auto",
+					bottom: 0,
+					paddingLeft: props.isDrawerOpen ? 280 : 0,
+				}}
+			>
+				<Toolbar variant="dense">
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={() => bottomLeftButtonAction(props)}
+						disabled={props.disableLeftButton}
+						style={{ display: props.leftButtonHide ? "none" : "block" }}
+					>
+						{props.loading ? (
+							<CircularProgress size={24} style={{ color: "white" }} />
+						) : (
+							props.leftButtonText
+						)}
+					</Button>
+					<div className={classes.grow} />
+					{props.otherActions}
+					<Button
+						variant="contained"
+						color="primary"
+						disabled={props.disableRightButton || props.loading}
+						onClick={() => bottomRightButtonAction(props)}
+						style={{
+							display: props.hideRightButton ? "none" : "flex",
+						}}
+					>
+						{props.loading ? (
+							<CircularProgress size={24} style={{ color: "white" }} />
+						) : (
+							props.right_button_text
+						)}
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
 
 BottomBar.propTypes = {
-  isDrawerOpen: PropTypes.bool,
-  disableLeftButton: PropTypes.bool,
-  leftButtonHide: PropTypes.bool,
-  leftButtonText: PropTypes.any,
-  bottomLeftButtonAction: PropTypes.func,
-  disableRightButton: PropTypes.bool,
-  hideRightButton: PropTypes.bool,
-  right_button_text: PropTypes.any,
-  bottomRightButtonAction: PropTypes.func,
-  loading: PropTypes.bool,
-  otherActions: PropTypes.any,
+	isDrawerOpen: PropTypes.bool,
+	disableLeftButton: PropTypes.bool,
+	leftButtonHide: PropTypes.bool,
+	leftButtonText: PropTypes.any,
+	bottomLeftButtonAction: PropTypes.func,
+	disableRightButton: PropTypes.bool,
+	hideRightButton: PropTypes.bool,
+	right_button_text: PropTypes.any,
+	bottomRightButtonAction: PropTypes.func,
+	loading: PropTypes.bool,
+	otherActions: PropTypes.any,
 };
 
 BottomBar.defaultProps = {
-  isDrawerOpen: true,
-  disableLeftButton: false,
-  leftButtonHide: false,
-  leftButtonText: "View",
-  bottomLeftButtonAction: (fn) => fn,
-  disableRightButton: false,
-  hideRightButton: false,
-  right_button_text: "Save",
-  bottomRightButtonAction: (fn) => fn,
-  loading: false,
-  otherActions: "",
+	isDrawerOpen: true,
+	disableLeftButton: false,
+	leftButtonHide: false,
+	leftButtonText: "View",
+	bottomLeftButtonAction: (fn) => fn,
+	disableRightButton: false,
+	hideRightButton: false,
+	right_button_text: "Save",
+	bottomRightButtonAction: (fn) => fn,
+	loading: false,
+	otherActions: "",
 };
 
 export default BottomBar;
